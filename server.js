6fb38f9d8a182ac7788b72ce35bc655f6c705f14
Chryssa <chryssajones@gmail.com>
2017-11-04 09:42:19 -0700

    more html

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
+// dependencies
+// ===============================
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 
+
+// starts express
+// ======================================
 var app = express();
-var port = 8080;
+var PORT = process.env.PORT || 8080;
 
-app.use(express.static("public"));+
+app.use(express.static("public"));
 
+
+// sets up body-parser for form input
+// ====================================
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.text());
 app.use(bodyParser.json());
 
