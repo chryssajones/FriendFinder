1f7200b99da0b6cd228a787d25559eb93bbe20ab
Chryssa <chryssajones@gmail.com>
2017-11-04 09:02:41 -0700

    html files created

---
+++ app/data/friends.js
@@ -0,0 +1,18 @@
+var friends = [
+	{
+  "name":"Chryssa",
+  "photo":"../public/assets/profile.png",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+}
+];
---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+var port = 8080;
+
+app.use(express.static("public"));+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
