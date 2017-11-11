af711c164cdbaa9069dd9615d917dd409403e8eb
Chryssa <chryssajones@gmail.com>
2017-11-11 00:11:35 -0800

    updated relative paths

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,13 @@
+// load dependencies
+var path = require("path");
+
+// routes
+module.exports = function(app) {
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/survey.html"));
+  });
+
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/home.html"));
+  });
+};
--- server.js
+++ server.js
@@ -1,22 +1,27 @@
-// dependencies
-// ===============================
+// load dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
-var path = require("path");
+var jsonParser = bodyParser.json();
 
-
-// starts express
-// ======================================
+// set up express
 var app = express();
 var PORT = process.env.PORT || 8080;
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + '/public'));
+
+// start the router
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 
-// sets up body-parser for form input
-// ====================================
+// set up body-parser for form input
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
-app.use(bodyParser.json());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// start listening!
+app.listen(PORT, function() {
+	console.log("app is listening on port " + PORT);
+});
 
